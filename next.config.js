fd79e01abf1cbaab6fba600289a21518daa1f5e3
ajshooting <ajshooting.aj@gmail.com>
2024-11-17 17:10:09 +0900

    ->githubPages

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/blog",
+    // ↓ next build を実行する際に Static Export を利用します。
+    output: 'export',
+    images: { unoptimized: true } 
+}
+
+module.exports = nextConfig
