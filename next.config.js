644ab87bbc8d298e8404ad909c6324bcd37a40bc
ajshooting <ajshooting.aj@gmail.com>
2024-11-17 17:28:09 +0900

    ->SSG

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
     basePath: "/blog",
     // ↓ next build を実行する際に Static Export を利用します。
     output: 'export',
-    images: { unoptimized: true } 
 }
 
 module.exports = nextConfig
