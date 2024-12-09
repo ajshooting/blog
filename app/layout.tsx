8c0f1f72e736cf4f536e9053600587bdbabca6ac
ajshooting <ajshooting.aj@gmail.com>
2024-12-09 13:18:23 +0900

    ->ja-JP

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     url: './',
     siteName: siteMetadata.title,
     images: [siteMetadata.socialBanner],
-    locale: 'en_US',
+    locale: 'ja-JP',
     type: 'website',
   },
   alternates: {
