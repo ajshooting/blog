2fb7170ccfc3f0e560fa839778aac5bf55467425
ajshooting <ajshooting.aj@gmail.com>
2024-12-11 16:38:56 +0900

    ->me

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,19 +8,11 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    description: `あなたはGoogleという検索エンジンを使ったことがありますか？
+    それはとても便利な検索エンジンです。
+    あなたも使ってみませんか？`,
     imgSrc: '/static/images/google.png',
     href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
   },
 ]
 
