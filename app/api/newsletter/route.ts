d37c2c9186ff6dea47290b6197b006693ac7f82f
ajshooting <ajshooting.aj@gmail.com>
2025-01-05 17:08:30 +0900

    やっぱりnewsletterは使わない。お金かかりそうだし

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,11 +1,13 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+// envやらなんやらやらないといけなそうなので一旦パス
 
-export const dynamic = 'force-static'
+// import { NewsletterAPI } from 'pliny/newsletter'
+// import siteMetadata from '@/data/siteMetadata'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+// export const dynamic = 'force-static'
 
-export { handler as GET, handler as POST }
+// const handler = NewsletterAPI({
+//   // @ts-ignore
+//   provider: siteMetadata.newsletter.provider,
+// })
+
+// export { handler as GET, handler as POST }
